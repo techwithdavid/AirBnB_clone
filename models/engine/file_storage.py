2f1c3842da48216ed087ee37ea2dda2ededc749c
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-05-11 18:58:40 -0700

    Update: Added all class attributes and instance methods

---
+++ models/engine/file_storage.py
@@ -0,0 +1,13 @@
+class FileStorage:
+
+    __file_path = ''
+    __objects = {}
+
+    def all(self):
+        pass
+    def new(self, obj):
+        pass
+    def save(self):
+        pass
+    def reload(self):
+        pass
